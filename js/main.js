86af2531eb782c34716b8e9987d80c3d9aa22051
Ramidi Priyanka <ramidipriyanka@unknown.home>
2017-12-02 21:42:24 +1100

    Added logic to game if cards flipped greater than two

--- js/main.js
+++ js/main.js
@@ -1,8 +1,39 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+//Storing the variables in an array
+var cards = ["queen", "queen", "king", "king"];
+//Storing the value of cards that are actively in play
+var cardsInPlay = [];
+
+//Storing value of first card flipped
+var cardOne;
+cardOne = cards[0];
+//Push it to cards in play array
+cardsInPlay.push(cardOne);
+console.log("User flipped card " + cardsInPlay);
+
+//Storing value of second card flipped
+var cardTwo;
+cardTwo = cards[2];
+//Push it to cards in play array
+cardsInPlay.push(cardTwo);
+console.log("User flipped card " + cardsInPlay[cardsInPlay.length - 1] );
+
+
+
+//Checking if number of cards flipped is greater than 2
+if(cardsInPlay.length === 2){
+	//if cards>2 & cards are same
+	if(cardsInPlay[0] === cardsInPlay[1]){
+		alert("You found a match!!!");
+	}
+	//if cards >2 and cards are not same
+	else {
+		alert("Sorry, try again");
+	}
+}
+
+else{
+	alert("Pick another card!")
+
+}
