e73c3bb074d4fecc303d807ddee6d8adb8cbc282
Ramidi Priyanka <ramidipriyanka@ramidis-mbp.home>
2017-12-03 20:02:30 +1100

    Changes done to few variables. Used arrays of objects

--- js/main.js
+++ js/main.js
@@ -1,7 +1,30 @@
 console.log("Up and running!");
 
-//Storing the variables in an array
-var cards = ["queen", "queen", "king", "king"];
+//Storing the card variables in an array using array & objects
+var cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+
+	{
+		rank: "king",
+		suit: "diamond",
+		cardImage: "images/king-of-diamond.png"
+	}
+];
 //Storing the value of cards that are actively in play
 var cardsInPlay = [];
 
@@ -11,11 +34,11 @@ var cardsInPlay = [];
 var checkForMatch = function(){
 	//if cards>2 & cards are same
 	if(cardsInPlay[0] === cardsInPlay[1]){
-		console.log("You found a match!!!");
+		alert("You found a match!!!");
 	}
 	//if cards >2 and cards are not same
 	else {
-		console.log("Sorry, try again");
+		alert("Sorry, try again");
 	}
 }
 
@@ -25,12 +48,13 @@ var checkForMatch = function(){
 var flipCard = function (cardId){
 
 		//Printing the card user flipped
-		console.log("User flipped card " +cardId);
+		console.log("User flipped card" +cards[cardId].rank);
+		console.log("of " +cards[cardId].suit);
+		console.log("(img) " +cards[cardId].cardImage);
 
 		//Push it to cards in play array
-		cardsInPlay.push(cards[cardId]);
+		cardsInPlay.push(cards[cardId].rank);
 		//console.log("User flipped card " + cardsInPlay[cardsInPlay.length - 1] );
-
 
 		//Checking if number of cards flipped is greater than 2
 		if(cardsInPlay.length === 2){
