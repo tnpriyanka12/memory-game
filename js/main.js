3c40feb0962c0205f4e7a3246a5a42ff66a0c2a7
tnpriyanka12 <tnpriyanka12@gmail.com>
2017-12-10 15:02:11 +1100

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,28 +1,28 @@
-console.log("Up and running!");
+console.log('Up and running!');
 
 //Storing the card variables in an array using array & objects
 var cards = [
 	{
-		rank: "queen",
-		suit: "hearts",
-		cardImage: "images/queen-of-hearts.png"
+		rank: 'queen',
+		suit: 'hearts',
+		cardImage: 'images/queen-of-hearts.png'
 	},
 
 	{
-		rank: "queen",
-		suit: "diamonds",
-		cardImage: "images/queen-of-diamonds.png"
+		rank: 'queen',
+		suit: 'diamonds',
+		cardImage: 'images/queen-of-diamonds.png'
 	},
 	{
-		rank: "king",
-		suit: "hearts",
-		cardImage: "images/king-of-hearts.png"
+		rank: 'king',
+		suit: 'hearts',
+		cardImage: 'images/king-of-hearts.png'
 	},
 
 	{
-		rank: "king",
-		suit: "diamond",
-		cardImage: "images/king-of-diamond.png"
+		rank: 'king',
+		suit: 'diamond',
+		cardImage: 'images/king-of-diamond.png'
 	}
 ];
 //Storing the value of cards that are actively in play
@@ -34,39 +34,49 @@ var cardsInPlay = [];
 var checkForMatch = function(){
 	//if cards>2 & cards are same
 	if(cardsInPlay[0] === cardsInPlay[1]){
-		alert("You found a match!!!");
+		alert('You found a match!!!');
 	}
 	//if cards >2 and cards are not same
 	else {
-		alert("Sorry, try again");
+		alert('Sorry, try again');
 	}
-}
+};
 
 //Function for flipping card
 //function
 //
-var flipCard = function (cardId){
+var flipCard = function (){
 
-		//Printing the card user flipped
-		console.log("User flipped card" +cards[cardId].rank);
-		console.log("of " +cards[cardId].suit);
-		console.log("(img) " +cards[cardId].cardImage);
+		var cardId ;
+		cardId = this.getAttribute('data-id');
 
 		//Push it to cards in play array
 		cardsInPlay.push(cards[cardId].rank);
-		//console.log("User flipped card " + cardsInPlay[cardsInPlay.length - 1] );
+		this.setAttribute('src', cards[cardId].cardImage);
 
 		//Checking if number of cards flipped is greater than 2
 		if(cardsInPlay.length === 2){
 			checkForMatch();
 		}
-
 		else {
-			console.log("Pick another card!");
+			console.log('Pick another card!');
 		}
 
 };
 
-flipCard(0);
-flipCard(2);
+//FUNCTION - Create Board
+var createBoard = function() {
+	for(var i=0; i<cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+
+	}
+};
+
+createBoard();
+//flipCard(0);
+//flipCard(2);
 
